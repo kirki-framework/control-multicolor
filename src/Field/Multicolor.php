008a3c1d113fe2ea33657d8fdfc216f544ab2f8c
Philipp <32090713+ouun@users.noreply.github.com>
2019-11-07 16:49:06 +0100

    Prevent breaking with wrong naming conventions
    Prevents PHP error if array key from `$args[ 'choices '][ $key ]` and `$args[ $setting ][ $key ]` are different. Until now it was not unset from the array as it was not found.

--- src/Field/Multicolor.php
+++ src/Field/Multicolor.php
@@ -103,8 +103,10 @@ class Multicolor extends Field {
 
 		// Unset input_attrs of all other choices
 		foreach ( $args['choices'] as $id => $set ) {
-			if ( $id !== $choice ) {
+			if ( $id !== $choice && isset( $args[ $setting ][ $id ] ) ) {
 				unset( $args[ $setting ][ $id ] );
+			} else if ( ! isset( $args[ $setting ][ $id ] ) ) {
+				$args[ $setting ] = '';
 			}
 		}
 
